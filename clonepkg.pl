d16bd7e50803807388fb7f2f3bd833e2e9a7965d
Andy Weidenbaum <archbaum@gmail.com>
2017-10-28 09:14:38 -0700

    rewrite in perl6

--- clonepkg.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-use 5.022;
-while (defined(my $pkg = shift(@ARGV)))
-{
-    chomp($pkg);
-    system("git", "clone", "https://aur.archlinux.org/$pkg.git");
-}
